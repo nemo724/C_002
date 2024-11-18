f093784f7e3c9334cef267a9b0be651dc87947cc
nemo <nemo724@naver.com>
2024-11-18 21:59:45 +0900

    srand 함수 및 난수 10개 생성함수 make_srand 작성

--- 002번/a002_random.c
+++ 002번/a002_random.c
@@ -6,3 +6,22 @@ int a[10];
 int dice[30];
 int rand_min = 1;
 int rand_max = 6;
+
+void make_srand(void);
+
+void make_srand(void)
+{for(int i =10;i<10;i++)
+	{a[i]=rand(); }
+}
+
+
+int main(void)
+{srand(time(0));
+
+
+
+
+
+
+return 0;
+}
