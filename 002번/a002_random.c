bc77b84a3103184f429319503ae0bc8e1fa7df8e
nemo <nemo724@naver.com>
2024-11-18 22:03:10 +0900

    생성한 10개의 난수를 출력하는 함수 print_srand 작성

--- 002번/a002_random.c
+++ 002번/a002_random.c
@@ -8,12 +8,18 @@ int rand_min = 1;
 int rand_max = 6;
 
 void make_srand(void);
+void print_srand(void);
 
 void make_srand(void)
 {for(int i =10;i<10;i++)
 	{a[i]=rand(); }
 }
 
+void print_srand(void)
+{for(int i = 0; i<10;i++)
+	{printf("%6d ", a[i]);}
+printf("\n");
+}
 
 int main(void)
 {srand(time(0));
